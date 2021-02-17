f04f48c0b3c89c49c588ff982626d28adb300385
cSDes1gn <christian911@sympatico.ca>
2021-02-17 14:18:44 -0500

    use host container instead of lxd

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ source .env
 # perl -pi -e 's/GIT_SHA = .*/GIT_SHA = "${SHA}"/g' ./monitor/__version__.py
 
 # clean snapcraft build container
-snapcraft clean --use-lxd;
+snapcraft clean;
 
 # build snapcraft in lxc container
-snapcraft --use-lxd --debug;
+snapcraft --debug;
 
 # push .snap file to s3 bucket
 ./s3_push.py -i $TARGET_FILE -o $OBJECT -b $BUCKET
