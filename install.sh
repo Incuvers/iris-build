b8df68faa365cba0d0b5bfe83c0a2b295b4b4c2e
cSDes1gn <christian911@sympatico.ca>
2021-02-18 17:46:07 -0500

    add auto keygen

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -l
 
+# TO ADD TO ANSIBLE PLAYBOOK FOR SERVER DEPLOYMENT
+ssh-keygen -b 4096 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
+
 # install docker
 curl -sSL https://get.docker.com | sh
 # add user to docker group
