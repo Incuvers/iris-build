c176eb264e4d2775e267ef6a696845ce98355222
cSDes1gn <christian911@sympatico.ca>
2021-02-18 14:51:17 -0500

    update docker install for arm64

--- install.sh
+++ install.sh
@@ -1,18 +1,7 @@
 #!/bin/bash -l
 
-sudo apt update
-# add Docker repository to APT sources:
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-# add the GPG key for the official Docker repository
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# add the Docker repository to APT sources
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-# add update apt cache
-sudo apt update
-# verify install from the Docker repo instead of the default Ubuntu repo
-apt-cache policy docker-ce
 # install docker
-sudo apt install docker-ce
+curl -sSL https://get.docker.com | sh
 # add user to docker group
 sudo usermod -aG docker "$USER"
 # reboot to apply user mode
