a946d36670f5ded515f642d686765f03e5364042
cSDes1gn <christian911@sympatico.ca>
2021-02-24 16:16:28 -0500

    sc2086

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
 # TO ADD TO ANSIBLE PLAYBOOK FOR SERVER DEPLOYMENT
-ssh-keygen -b 4096 -t rsa -f $HOME/.ssh/id_rsa -q -N ""
+ssh-keygen -b 4096 -t rsa -f "$HOME"/.ssh/id_rsa -q -N ""
 
 # install docker
 curl -sSL https://get.docker.com | sh
