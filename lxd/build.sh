23e1854aea0a9a61f1c741a8164d0be4e9a5ed68
cSDes1gn <christian911@sympatico.ca>
2021-04-15 18:19:23 -0400

    build prod and staging snap concurrently

--- lxd/build.sh
+++ lxd/build.sh
@@ -19,6 +19,14 @@ handler () {
     fi
 }
 
+notify() {
+    # Notify slack channel of build success
+    printf "%b" "${OKB}Notifying slack channel of snap build success.${NC}\n"
+    curl -X POST -H 'Content-type: application/json' \
+        --data "{\"text\":\"$STAGE Snap build server job complete. Build logs: https://github.com/Incuvers/monitor/actions/workflows/iris.yml. Snap file: https://s3.console.aws.amazon.com/s3/buckets/snapbuilds?region=ca-central-1&tab=objects\"}" https://hooks.slack.com/services/"$SLACK_IDENTIFIER"
+    printf "%b" "${OKG} ✓ ${NC}complete"
+}
+
 python3 -m pip install pyyaml
 
 printf "%b" "${OKB}Starting snap build job${NC}\n"
@@ -26,16 +34,19 @@ printf "%b" "${OKB}-----------------------${NC}\n"
 VERSION=$(echo "$GIT_REF" | awk -F '/' '{print $3}' | cut -c2-)
 # export to env for yaml parse
 export VERSION="$VERSION"
-export STAGE="$STAGE"
-printf "%b" "${OKB}Release: ${VERSION}${NC}\n"
-printf "%b" "${OKB}Stage: ${STAGE}${NC}\n"
-
+export STAGE="https://api.staging.incuvers.com"
 SNAP_ARCH="arm64"
 # Required for aws s3 push script
 TARGET_FILE="iris-incuvers_${VERSION}_${SNAP_ARCH}.snap"
 BUCKET="snapbuilds"
-OBJECT="iris-incuvers.snap"
+printf "%b" "${OKB}Release: ${VERSION}${NC}\n"
+printf "%b" "${OKB}Bucket: ${STAGE}${NC}\n"
+printf "%b" "${OKB}Arch: ${SNAP_ARCH}${NC}\n"
+printf "%b" "${OKB}File: ${STAGE}${NC}\n"
 
+# Required for aws s3 push script
+OBJECT="iris-incuvers-staging.snap"
+printf "%b" "${OKB}Starting snap build for ${OKG}$OBJECT${NC}\n"
 printf "%b" "${OKB}Populating the snapcraft buildspec${NC}\n"
 ./yaml_parser.py -i snap/snapcraft.yaml
 printf "%b" "${OKG} ✓ ${NC}complete\n"
@@ -56,8 +67,27 @@ printf "%b" "${OKB}Pushing $TARGET_FILE to S3 bucket $BUCKET as $OBJECT${NC}\n"
 ./s3_push.py -i "$TARGET_FILE" -o "$OBJECT" -b "$BUCKET"
 printf "%b" "${OKG} ✓ ${NC}complete\n"
 
-# Notify slack channel of build success
-printf "%b" "${OKB}Notifying slack channel of snap build success.${NC}\n"
-curl -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"Snap build server job complete. Build logs: https://github.com/Incuvers/monitor/actions/workflows/iris.yml. Snap file: https://s3.console.aws.amazon.com/s3/buckets/snapbuilds?region=ca-central-1&tab=objects\"}" https://hooks.slack.com/services/"$SLACK_IDENTIFIER"
-printf "%b" "${OKG} ✓ ${NC}complete"
+printf "%b" "${OKB}Remove $STAGE snap build artefact${NC}\n"
+rm -f -- *.snap
+printf "%b" "${OKG} ✓ ${NC}complete\n"
+notify()
+
+# start production snap build phase
+export STAGE="https://api.prod.incuvers.com"
+OBJECT="iris-incuvers-prod.snap"
+printf "%b" "${OKB}Starting snap build for ${OKG}$OBJECT${NC}\n"
+printf "%b" "${OKB}Populating the snapcraft buildspec${NC}\n"
+./yaml_parser.py -i snap/snapcraft.yaml
+printf "%b" "${OKG} ✓ ${NC}complete\n"
+
+# build snapcraft in using host container
+printf "%b" "${OKB}Starting snap build on host container${NC}\n"
+snapcraft --use-lxd --bind-ssh
+printf "%b" "${OKG} ✓ ${NC}complete\n"
+
+# push .snap file to s3 bucket
+printf "%b" "${OKB}Pushing $TARGET_FILE to S3 bucket $BUCKET as $OBJECT${NC}\n"
+./s3_push.py -i "$TARGET_FILE" -o "$OBJECT" -b "$BUCKET"
+printf "%b" "${OKG} ✓ ${NC}complete\n"
+notify()
+
