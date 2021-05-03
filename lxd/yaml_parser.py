e9a1fb0b93ff13de352e30022e6617dd4d4941e8
cSDes1gn <christian911@sympatico.ca>
2021-05-03 10:45:13 -0400

    update yaml parser to use direct dictionary insertion rather than path matching

--- lxd/yaml_parser.py
+++ lxd/yaml_parser.py
@@ -14,13 +14,12 @@ test.yaml:
     ...
 If the environment variable cannot be found a default value can be specified as a fallback.
 """
-import pprint
-import logging
-import yaml
-import re
 import os
 import sys
+import yaml
 import getopt
+import pprint
+import logging
 
 def main(argv):
     snap_file = ''
@@ -35,7 +34,7 @@ def main(argv):
             sys.exit()
         elif opt in ("-i", "--input"):
             snap_file = arg
-    
+ 
     logging.info("Loading yaml from: %s", snap_file)
     # LOAD path to yaml from arg
     with open(snap_file, 'r') as stream:
@@ -46,36 +45,17 @@ def main(argv):
         except IndexError as exc:
             logging.exception("Environment variable required but was not specified: %s", exc)
         else:
+            # manual resolution of version and endpoint  
+            snap['version'] = os.environ.get('VERSION')
+            snap['apps']['monitor']['API_BASE_URL'] = os.environ.get('STAGE')
             logging.info('Saving to build directory')
             with open(snap_file, 'w') as outstream:
                 yaml.dump(snap, outstream, default_flow_style=False)
         logging.info("Yaml parse complete:\n%s", pprint.pformat(snap))
-
-def path_constructor(loader:yaml.loader.SafeLoader, node:yaml.nodes.ScalarNode) -> str:
-    """
-    Extract the matched value, expand env variable, and replace the match
-    
-    :raises IndexError: if environment variable was not found and no default was specified
-    :returns: string replacement
-    """
-    value = node.value
-    logging.info("Value: %s", value)
-    match = path_matcher.match(value)
-    env_var = match.group()[2:-1].split(':')
-    logging.info("Env: %s", env_var)
-    try:
-        var = os.environ[env_var[0]] + value[match.end():]
-    except KeyError:
-        # here we expect the default to be defined otherwise we raise IndexError
-        var = env_var[1] + value[match.end():]
-    return var
 
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s %(levelname)s %(name)s %(message)s",
         level=logging.INFO
     )
-    path_matcher = re.compile(r'\$\{([^}^{]+)\}')
-    yaml.add_implicit_resolver('!path', path_matcher, None, yaml.loader.SafeLoader)
-    yaml.add_constructor('!path', path_constructor, yaml.loader.SafeLoader)
     main(sys.argv[1:])
